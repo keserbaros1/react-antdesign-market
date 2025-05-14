77a4e1901d9487ea225f23920431bba0188c77af
keserbaros1 <keserbaros1@hotmail.com>
2025-05-14 22:25:21 +0300

    kullanıcı adı ve şifre doğruysa giriş sağlanması eklendi

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dashboard';
 
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = (username) => {
+
+  const handleLogin = async (username, password) => {
     setUser(username);
+    setPassword(password);
     setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setUser("");
+    setPassword("");
+    setIsLoggedIn(false);
   };
 
   return (
     <div className="App">
       {isLoggedIn ? (
-        <Dashboard user={user} />
+        <Dashboard user={user} onLogout={handleLogout} />
       ) : (
         <LoginForm onLogin={handleLogin} />
       )}
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,37 @@
 // src/components/LoginForm.js
 import { Form, Input, Button, Card } from 'antd';
+import { login } from '../api/googleApi';
+
 
 export default function LoginForm({ onLogin }) {
-  const onFinish = (values) => {
-    const { username } = values;
-    onLogin(username);
+  const onFinish = async (values) => {
+    const { username, password } = values;
+    const success = await login(username, password);
+    if (success) {
+      onLogin(username, password);
+    } else {
+      alert("Kullanıcı adı veya şifre hatalı!");
+    }
   };
 
   return (
-    <Card title="Market Otomasyonu Giriş" style={{ maxWidth: 400, margin: 'auto', marginTop: 100 }}>
-      <Form onFinish={onFinish}>
-        <Form.Item name="username" rules={[{ required: true, message: 'Kullanıcı adı gerekli!' }]}>
+    <Card 
+      title="Market Otomasyonu Giriş" 
+      style={{ maxWidth: 400, margin: 'auto', marginTop: 100 }}>
+      
+      <Form onFinish={onFinish}>        
+        <Form.Item 
+          name="username" 
+          rules={[{ required: true, message: 'Kullanıcı adı gerekli!' }]}>
           <Input placeholder="Kullanıcı Adı" />
         </Form.Item>
+        
+        <Form.Item
+          name="password" 
+          rules={[{ required: true, message: 'Şifre gerekli!' }]}>
+          <Input.Password placeholder="Şifre" />
+        </Form.Item>
+        
         <Form.Item>
           <Button type="primary" htmlType="submit" block>Giriş Yap</Button>
         </Form.Item>
